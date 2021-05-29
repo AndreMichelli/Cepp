388cf98810ec37ce0b1730da257482f016a2aa9e
André Picaro Michelli <andremichelli0108@gmail.com>
2021-05-29 18:17:54 -0300

    second commit

---
+++ cepp/src/components/ListaDeNotas.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import CardNota from "./CardNota/CardNota";
+
+class ListaDeNotas extends Component {
+  render() {
+    return (
+      <ul>
+        {Array.of("Trabalho", "Trabalho", "Estudos").map((categoria, index) => {
+          return (
+            <li key = {index}>
+              <CardNota />
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+}
+
+export default ListaDeNotas;
